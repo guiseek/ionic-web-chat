c0df497c19e6aa5772e975949d7e380633f6c71e
Gui Seek <guiseek@gmail.com>
2018-05-20 04:02:18 -0300

    base

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDvaar_7Tz4Xc4h9JH0JYP_UKvZwfy_t9g",
+    authDomain: "fcv-chat.firebaseapp.com",
+    databaseURL: "https://fcv-chat.firebaseio.com",
+    projectId: "fcv-chat",
+    storageBucket: "fcv-chat.appspot.com",
+    messagingSenderId: "701220424157"
+  }
+};
