47baae7310733e16f0e8cbddbee2544c81322d82
Gui Seek <guiseek@gmail.com>
2018-05-21 02:10:41 -0300

    Login Page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl } from '@angular/forms';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  user = new FormGroup({
+    nickname: new FormControl()
+  });
+  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  enter() {
+    window.localStorage.setItem('user',this.user.get('nickname').value);
+    // this.navCtrl.setRoot(TabsPage);
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+}
